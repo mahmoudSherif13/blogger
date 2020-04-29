35df1f51132a1d50ed5ae047d9f1b81b1443efef
mahmoodSherif <ma13121999@gmail.com>
2020-04-29 05:51:14 +0200

    add tags

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,4 +39,5 @@ class ArticlesController < ApplicationController
         flash.notice = "Article #{@article.title} updated!"
         redirect_to article_path(@article)
     end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings 
+
+    def tag_list
+        tags.join(", ")
+    end
+
+    def tag_list=(tags_string)
+        tag_names = tags_string.split(",").map{|s| s.strip.downcase()}.uniq()
+        tags = tag_names.map{ |name| Tag.find_or_create_by(name: name) }
+        self.tags = tags
+    end
 end
